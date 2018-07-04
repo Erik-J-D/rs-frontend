8e44a93bc1cf1ce796cf032aea3e0281ad992097
Erik Derohanian <erik@erik-j.de>
2018-07-04 19:42:18 -0400

    test flexbox

---
+++ rs-frontent/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class PlantCard extends Component {
+  render() {
+    return (
+      <div className="plant-card">
+        <p>Name: {this.props.data.name}</p>
+        <p>Moisture: {this.props.data.moisture}</p>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  makeFakePlant() {
+    return {
+      name: Math.random().toString(36).slice(2, 7),
+      moisture: Math.round((Math.random() * 100)).toString() + '%'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Sample Plant Cards</h1>
+        </header>
+
+        <div className="card-container">
+          {Array(10).fill(0).map( (_, i) => <PlantCard data={this.makeFakePlant()} key={i} /> )}
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
